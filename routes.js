af814c6324bf4a47bb8ce59f9944bbf5a6e1c9c7
jdflorez39 <jd.florez39@gmail.com>
2014-04-15 14:58:37 -0500

    index jaded

--- routes.js
+++ routes.js
@@ -3,6 +3,10 @@ var mailer = require('./mailers/mailer');
 module.exports = function(app) {
 
 app.get('/', function(req, res) {
+	res.render('index'); 
+});
+
+app.get('/portfolio', function(req, res) {
 	res.render('portfolio'); 
 });
 
