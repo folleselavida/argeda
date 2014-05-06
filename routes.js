9badcc736bf813c256cc90272ba023498a7bd080
antwort7 <antwort7@gmail.com>
2014-05-06 01:25:29 -0500

    advanced landing

--- routes.js
+++ routes.js
@@ -3,7 +3,7 @@ var mailer = require('./mailers/mailer');
 module.exports = function(app) {
 
 app.get('/', function(req, res) {
-	res.render('index'); 
+	res.render('landing'); 
 });
 
 app.get('/portfolio', function(req, res) {
